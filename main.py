925731760262d5af552b197f33fe5ef02aaa89a3
AmirWinchester <37380166+AmirWinchester@users.noreply.github.com>
2023-09-18 13:07:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World Amir Winchester'
+    return 'Hello World Amir Winchester into Kaman\n'
 
 @app.errorhandler(500)
 def server_error(e):
